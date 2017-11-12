adc20363e1efee6eac55253277d20a6d309ebbf6
Juli Cheng <teachjuli@gmail.com>
2017-11-12 15:11:39 +0000

    add event listener when click on cell

--- 21 Project Pixel Art Maker/designs.js
+++ 21 Project Pixel Art Maker/designs.js
@@ -13,3 +13,9 @@ document.getElementById("colorPicker").addEventListener("change", function(){
 	//when user changes the colour picker then assign color variable to new change
 	color = document.getElementById("colorPicker").value;
 });
+
+//select cell to be coloured event listener
+document.getElementById("pixel_canvas").addEventListener("click", function(evt){
+	//change the colour of the cell that was clicked
+	evt.target.style.backgroundColor = color;
+});
